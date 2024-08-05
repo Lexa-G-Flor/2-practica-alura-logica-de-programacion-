b52ca1b65e7ff606bfdcc41d7060cdea3a9a7a39
Lexa-G-Flor <alexandragonzalezflores@hotmail.com>
2024-08-05 04:44:47 -0400

    agrega atributo onclick(html) a botones para conectar funciones JavaScript

--- interactuando-con-html/clases/Aula1/app.js
+++ interactuando-con-html/clases/Aula1/app.js
@@ -4,3 +4,7 @@ titulo.innerHTML = 'Juego del numero secreto';
 
 let parrafo = document.querySelector ('p');
 parrafo.innerHTML = 'Indica un numero del 1 al 10:';
+
+function intentoDeUsuario () {
+    alert ('click desde la funcion o boton');
+}
