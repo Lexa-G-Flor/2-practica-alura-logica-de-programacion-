3d623135c809cd96ed6fe4200c5c1fefc06b0354
Lexa-G-Flor <alexandragonzalezflores@hotmail.com>
2024-08-13 02:25:41 -0400

    Refactorizar función de verificación de intento
    03 reiniciandoeljuego
    
    1. 02Mensaje, si acertó1.	02Mensaje, si acertó
    
    Significa que hiciste cambios en la función verificarIntento() para mejorar su estructura, legibilidad o eficiencia, sin cambiar su comportamiento.
    
    En este caso, se eliminó la variable mensaje y se simplificó la estructura del código, haciendo que sea más fácil de leer y entender.

--- 3reiniciando-el-juego/practicas/practica-1/app.js
+++ 3reiniciando-el-juego/practicas/practica-1/app.js
@@ -12,72 +12,24 @@ elementoHtml.innerHTML = texto;
 return; 
 }
 
-//APUNTES - 
-
-/*  1. Cambio de nombre de la funcion y verificacion en index.html
-
-let numeroDeUsuario = document.querySelector ('input')
- 
-- Se cambia el nombre intentoUsurio por verificarIntento
-- Recordar cambiar tambien donde se llama la funcion en index.html
-- etiqueta input representa la caja de texto. Se encuentra en index.html
-- document.querySelector es una función permite seleccionar un elemento HTML.
-- Cuando solo hay un input se puede utilizar querySelector 
-
-function verificarIntento () {
-    let numeroDeUsuario = document.querySelector ('input')
-    return; 
-}
-
-*/
-
-/* 2. cambio del DOC y asignacion de id, del value
-
-let numeroDeUsuario = document.getElementById ('valorUsuario').value;
-
-- Pero cuando hay mas de un input se les asigna un id o nombre para buscarla
-- Donde se utiliza document.getElementById ('id');
-- este retorna el objeto
-- Para solo ver el valor al final se agrega un punto y value
-- document.getElementById ('valorUsuario').value;
-- Se puede tener mas de un input. Pero se utiliza id  
- 
-function verificarIntento () {
-    let numeroDeUsuario = document.getElementById ('valorUsuario');
-    return; 
-}
-
-*/
-
-/* 3. - Verificacion el tipo de dato, si numeroDeUsuario es un string o un número.
-        -Si es un string, puedes usar parseInt() en la variable 
-         para convertirlo a un número antes de realizar operaciones matemáticas.
-
-        let numeroDeUsuario = parseInt (document.getElementById ('valorUsuario').value);
-*/
-
-/* 4. Comparar valores y verificar tipo de dato
-
-console.log (typeof(numeroDeUsuario));
-console.log (typeof(numeroSecreto));
-
-- Muestra el valor de la variable en la consola del navegador.
-- Se compara los dos numeros retornando un booleano o true - false (tipo de dato booleano == )
-- Los tipos de datos booleanos son como interruptores que solo pueden estar en dos estados: verdadero o falso.
-- Y se utiliza el === para que sea igual en tipo y en valor  
-
-*/ 
 
 function verificarIntento () {
     let numeroDeUsuario = parseInt (document.getElementById ('valorUsuario').value);
+
+    console.log (numeroSecreto); 
+
+    if (numeroDeUsuario === numeroSecreto) {
+        asignarTextoElemento ('p', 'Acertaste el numero');
+    } else { 
+        if(numeroDeUsuario > numeroSecreto){
+        asignarTextoElemento ('p', 'El numero secreto es menor');
+        } else {
+        asignarTextoElemento ('p', 'El numero secreto es mayor');
+        }
+    }
+ 
+    }
     
-    console.log (typeof(numeroDeUsuario));
-    console.log (numeroDeUsuario);
-    console.log (typeof(numeroSecreto));
-    console.log(numeroSecreto); 
-    console.log (numeroDeUsuario === numeroSecreto); 
-    return; 
-}
 
 // La función genera un número aleatorio entre 1 y 10 y te lo devuelve.
 function generarNumeroSecreto() {
